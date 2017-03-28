13aff62184ecf5ba3d549a828955050f723d55cd
Brian Lindner <brian@bclindner.com>
2017-03-28 12:13:24 +0000

    safety commit before big changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var compression = require('compression')
+var helmet = require('helmet')
 var app = express()
 app.set('view engine', 'pug')
 app.use('/static',express.static('static'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(compression())
+app.use(helmet())
 
 //validator widget
 require('./routes/widgets/validate.js')(app,require('csslint').CSSLint)
@@ -16,6 +18,11 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(31415, () =>{
-  console.log('listening on port 31415')
+//nextsite page
+app.get('/next/', (req, res) => {
+	res.render('./next/index.pug')
+})
+
+app.listen(80, () =>{
+  console.log('listening on port 80')
 })
