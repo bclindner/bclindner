91e2fbbef4e7bdfd4a43c513b72927c1a0231acf
Brian Lindner <brian@bclindner.com>
2017-04-02 22:53:18 -0400

    blog system works somewhat; needs work.

--- routes/main/blog.js
+++ routes/main/blog.js
@@ -1,18 +1,19 @@
+var fs = require('fs')
+var posts = require('../../data/db.json').posts //static DB import
+var markdown = require('markdown').markdown
 module.exports = function(app){
-  db = require('../../controllers/blog.js')
-  //index page: display recent posts
-app.get('/blog',(req,res)=>{
-  res.render('blog/index.pug',{posts: posts})
-})
-app.get('/blog/post/:id', (req,res) => {
-    var postId = req.params.id
-    var post = require('../../data/db.json').posts[postId]
-    if(post){
-      //render the post markdown to HTML to be added to the template
+  // blog home page
+  app.get('/blog',(req,res)=>{
+    res.render('blog/index.pug',{posts: posts})
+  })
+  //blog post getter
+  app.get('/blog/post/:id', (req,res) => {
+    var post = posts[req.params.id] //get the post - we still have some processing to do
+    post.date = new Date(post.date*1000) //convert the date from UNIX time
+    fs.readFile("views/blog/posts/"+post.slug+".md", "utf-8",(err, data) =>{
+      if(err){throw err} //because what's the point of getting a post if the post doesn't have any content
+      post.content = markdown.toHTML(data)
       res.render('blog/viewpost.pug',post)
-    }
-    else{res.status(404).render('404.pug')}
-
-})
-
+    })
+  })
 }
