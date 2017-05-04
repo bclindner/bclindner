602a3b1b3687132282ca5551e4743a7f210781dc
Brian C. Lindner <brian@bclindner.com>
2017-05-04 17:30:02 -0400

    added node-sass-middleware requirement - whoops!

--- routes/main/blog.js
+++ routes/main/blog.js
@@ -2,6 +2,9 @@ var fs = require('fs')
 var posts = require('../../data/db.json').posts //static DB import
 var markdown = require('markdown').markdown
 module.exports = function(app){
+  for(i=0;i=posts.length;i++){
+    post.date = new Date(post.date*1000) //convert the date from UNIX time
+  }
   // blog home page
   app.get('/blog',(req,res)=>{
     res.render('blog/index.pug',{posts: posts})
@@ -9,11 +12,5 @@ module.exports = function(app){
   //blog post getter
   app.get('/blog/post/:id', (req,res) => {
     var post = posts[req.params.id] //get the post - we still have some processing to do
-    post.date = new Date(post.date*1000) //convert the date from UNIX time
-    fs.readFile("views/blog/posts/"+post.slug+".md", "utf-8",(err, data) =>{
-      if(err){throw err} //because what's the point of getting a post if the post doesn't have any content
-      post.content = markdown.toHTML(data)
-      res.render('blog/viewpost.pug',post)
-    })
   })
 }
