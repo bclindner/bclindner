835eaea3134e6473a3da5acea5c876ea7215347d
Brian C. Lindner <brian@bclindner.com>
2017-05-04 19:43:43 -0400

    blog system now working w/ Google Datastore

--- routes/main/blog.js
+++ routes/main/blog.js
@@ -1,16 +1,36 @@
 var fs = require('fs')
-var posts = require('../../data/db.json').posts //static DB import
-var markdown = require('markdown').markdown
+var datastore = require('@google-cloud/datastore')({ // initialize GCP Datastore
+  projectId: "bclindner-servers-166603",
+  keyFilename: "gcloudkey.json"
+})
+var postKind = "bclindnerPost"
 module.exports = function(app){
-  for(i=0;i=posts.length;i++){
-    post.date = new Date(post.date*1000) //convert the date from UNIX time
-  }
   // blog home page
   app.get('/blog',(req,res)=>{
-    res.render('blog/index.pug',{posts: posts})
+    // get 3 most recent posts
+    var recentPostQuery = datastore.createQuery(postKind)
+      .order('datePosted')
+      .limit(5)
+    datastore.runQuery(recentPostQuery)
+      .then((posts) => {
+	posts = posts[0] // come on, Google... seriously?
+	res.render('blog/index.pug',{posts: posts})
+      })
+      .catch((err) =>{
+	console.log("err: "+err)
+	res.sendStatus(500)
+      })
   })
   //blog post getter
-  app.get('/blog/post/:id', (req,res) => {
-    var post = posts[req.params.id] //get the post - we still have some processing to do
+  app.get('/blog/:slug', (req,res) => {
+    var getPostQuery = datastore.createQuery(postKind)
+      .filter('slug','=',req.params.slug)
+      .limit(1)
+    datastore.runQuery(getPostQuery)
+      .then((post) => {
+        post = post[0][0] // GOOGLE. COME ON. STOP.
+        console.log(post)
+        res.render("blog/posts/"+post.slug,{post: post})
+      })
   })
 }
